beedf7bd58705b2d3b5a0a2d995fc04edb8ac5ee
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 04:01:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,6 @@ var playlist = {'Social D':'story of my life'}
 function updatePlaylist(list,name,song) {
   playlist[name] = song
 }
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
